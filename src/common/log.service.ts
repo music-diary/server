7546f26fa8352e10e7a8be5322f1c4333f7500ac
minenam <nhm6947@gmail.com>
2024-05-24 05:19:02 +0900

    fix: change log service more visible

--- src/common/log.service.ts
+++ src/common/log.service.ts
@@ -2,39 +2,29 @@ import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class LogService extends Logger {
-  private readonly logger: any;
+  private readonly logger: Logger;
   constructor() {
     super();
     this.logger = new Logger();
   }
 
-  /**
-   * Write a 'log' level log.
-   */
-  log(message: any, ...optionalParams: any[]) {
-    this.logger.log(message, optionalParams);
-  }
-  /**
-   * Write an 'error' level log.
-   */
-  error(message: any, ...optionalParams: any[]) {
-    this.logger.error(message, optionalParams);
-  }
-  /**
-   * Write a 'debug' level log.
-   */
-  debug(message: any, ...optionalParams: any[]) {
-    this.logger.error(message, optionalParams);
+  log(message: any, context: string) {
+    this.logger.log(message, context);
   }
 
-  verbose(message: any, ...optionalParams: any[]) {
-    this.logger.verbose(message, optionalParams);
+  error(message: any, context: string) {
+    this.logger.error(message, context);
   }
 
-  warn(message: any, ...optionalParams: any[]) {
-    this.logger.warn(message, optionalParams);
+  debug(message: any, context: string) {
+    this.logger.debug(message, context);
   }
-  fatal(message: any, ...optionalParams: any[]) {
-    this.logger.log(message, optionalParams);
+
+  verbose(message: any, context: string) {
+    this.logger.verbose(message, context);
+  }
+
+  warn(message: any, context: string) {
+    this.logger.warn(message, context);
   }
 }
