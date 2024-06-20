bf5ae46091ed76fc09b447eecb9067aa5323d408
minenam <nhm6947@gmail.com>
2024-06-21 03:02:44 +0900

    fix: fix prisma exception filter

---
+++ src/filters/prisma-exception.filter.ts
@@ -0,0 +1,30 @@
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+import { Request, Response } from 'express';
+import { LogService } from 'src/common/log.service';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaExceptionFilter extends BaseExceptionFilter {
+  private readonly logger = new LogService();
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse<Response>();
+    const _request = context.getRequest<Request>();
+
+    const exceptionName = exception?.name;
+    const exceptionMessage = exception?.message;
+    const stack = exception?.stack;
+    this.logger.error(`${exceptionMessage}\n${stack}`, exceptionName);
+
+    if (exception.code === 'P2002') {
+      response.status(HttpStatus.CONFLICT).json({
+        statusCode: HttpStatus.CONFLICT,
+        message: exceptionMessage,
+        error: exceptionName,
+      });
+    } else {
+      super.catch(exception, host);
+    }
+  }
+}
