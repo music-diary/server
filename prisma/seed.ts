d0cdbfb6ac9709df14aa4d4aa8864596f6a80eeb
minenam <nhm6947@gmail.com>
2024-06-21 00:20:37 +0900

    feat: add genres seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -48,13 +48,55 @@ const userData: Prisma.UsersCreateInput[] = [
   // },
 ];
 
+const genresData: Prisma.GenresCreateInput[] = [
+  {
+    name: 'ballade',
+  },
+  {
+    name: 'dance',
+  },
+  {
+    name: 'hip-hop',
+  },
+  {
+    name: 'rnb',
+  },
+  {
+    name: 'indie',
+  },
+  {
+    name: 'rock',
+  },
+  {
+    name: 'pop',
+  },
+  {
+    name: 'new-age',
+  },
+  {
+    name: 'fork',
+  },
+  {
+    name: 'electronica',
+  },
+  {
+    name: 'ost',
+  },
+  {
+    name: 'jazz',
+  },
+  {
+    name: 'j-pop',
+  },
+];
+
 async function main() {
   console.log(`Start seeding ...`);
-  for (const u of userData) {
-    const user = await prisma.users.create({
-      data: u,
+  for (const genreData of genresData) {
+    const genre = await prisma.genres.create({
+      data: genreData,
     });
-    console.log(`Created user with id: ${user.id}`);
+    console.log(`Created genre with id: ${genre.id}`);
   }
   console.log(`Seeding finished.`);
 }
