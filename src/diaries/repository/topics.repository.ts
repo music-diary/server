03607e775306bb22e9906c8d5131b4721f304bdb
minenam <nhm6947@gmail.com>
2024-07-02 08:18:43 +0900

    feat: implement get topics API

---
+++ src/diaries/repository/topics.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, Topics } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
+
+@Injectable()
+export class TopicsRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findAll(query?: Prisma.TopicsFindManyArgs): Promise<Topics[]> {
+    return await this.prismaService.topics.findMany(query);
+  }
+}
